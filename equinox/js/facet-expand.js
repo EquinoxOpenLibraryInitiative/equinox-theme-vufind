14b8d61635d13a55cfe74e7b0995714e81c7233f
stephanieleary <steph@stephanieleary.com>
2023-02-07 16:26:59 +0000

    Add aria-expanded to facet dropdowns
    Signed-off-by: stephanieleary <steph@stephanieleary.com>

--- equinox/js/facet-expand.js
+++ equinox/js/facet-expand.js
@@ -8,33 +8,42 @@ $(document).ready(function () {
 
         // tabindex is needed to allow focus via keyboard
         $('h2', this).addClass('dropdown-toggle').attr('tabindex', 0);
+
         // the individual facet links should be removed from the tabbing order for now
         $('.home-facet-container li a', this).attr('tabindex', -1);
     });
 
-    // When opening, links are added back to the tabbing order
-    // When closing, links are removed from tabbing order
-    $('.dropdown-toggle').click(function() { 
-        $(this).next('.dropdown').slideToggle().toggleClass('open');
-        $('.dropdown li a').attr('tabindex', -1);
-        $('.dropdown.open li a').attr('tabindex', 0);
-    });
-
-    // Pressing enter on a focused H2 should toggle open/close
-    $('.dropdown-toggle').on('keydown', function(event) { 
-        if (event.key === "Enter") {
-            event.preventDefault();
-            $(this).next('.dropdown').slideToggle().toggleClass('open');
-            $('.dropdown li a').attr('tabindex', -1);
-            $('.dropdown.open li a').attr('tabindex', 0);
+    // Click or press enter or down arrow to toggle open/close
+    $('.dropdown-toggle').on('click keydown', function (event) {
+        if (event.type == "keydown") {
+            if (event.key == "Enter" || event.key == "ArrowDown") {
+                kbd_dropdown_toggle(event);
+            }
+        } else {
+            kbd_dropdown_toggle(event);
         }
     });
 
-    // Close the dropdown on Esc (anywhere in the document)
+    // Close all dropdowns on Esc (anywhere in the document)
     $(document).on('keydown', function(event) {
         if (event.keyCode == 27) {
             $('.dropdown.open').slideToggle().toggleClass('open');
             $('li a', this).attr('tabindex', -1);
         }
     });
+
+    function kbd_dropdown_toggle(event) {
+        event.preventDefault();
+        // Toggle open class and slide open
+        $(this).next('.dropdown').slideToggle().toggleClass('open');
+        
+        // Toggle ARIA states and force false attribute to be present
+        $(this).next('.dropdown').toggleAttribute('aria-expanded', true);
+        
+        // Remove from tabbing order any links in closed dropdowns
+        $('.dropdown li a').attr('tabindex', -1);
+        
+        // When opening, links are added back to the tabbing order
+        $('.dropdown.open li a').attr('tabindex', 0);
+    }
 });
