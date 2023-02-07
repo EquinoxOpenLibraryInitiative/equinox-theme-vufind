ec09d1713c5be6dddbcbef3a441830e2a674baf9
stephanieleary <steph@stephanieleary.com>
2023-02-07 17:55:16 +0000

    correct ARIA states for dropdowns
    Signed-off-by: stephanieleary <steph@stephanieleary.com>

--- equinox/js/facet-expand.js
+++ equinox/js/facet-expand.js
@@ -7,43 +7,62 @@ $(document).ready(function () {
         $('.home-facet-container', this).addClass('dropdown').css('width', w);
 
         // tabindex is needed to allow focus via keyboard
-        $('h2', this).addClass('dropdown-toggle').attr('tabindex', 0);
+        $('h2', this)
+            .addClass('dropdown-toggle')
+            .attr('tabindex', 0)
+            .attr('aria-expanded', false)
+            .attr('aria-haspopup', true);
 
         // the individual facet links should be removed from the tabbing order for now
         $('.home-facet-container li a', this).attr('tabindex', -1);
     });
 
-    // Click or press enter or down arrow to toggle open/close
-    $('.dropdown-toggle').on('click keydown', function (event) {
-        if (event.type == "keydown") {
-            if (event.key == "Enter" || event.key == "ArrowDown") {
-                kbd_dropdown_toggle(event);
-            }
-        } else {
-            kbd_dropdown_toggle(event);
-        }
-    });
-
-    // Close all dropdowns on Esc (anywhere in the document)
-    $(document).on('keydown', function(event) {
-        if (event.keyCode == 27) {
-            $('.dropdown.open').slideToggle().toggleClass('open');
-            $('li a', this).attr('tabindex', -1);
-        }
-    });
-
-    function kbd_dropdown_toggle(event) {
-        event.preventDefault();
-        // Toggle open class and slide open
+    // When opening, links are added back to the tabbing order
+    // When closing, links are removed from tabbing order
+    $('.dropdown-toggle').click(function() { 
         $(this).next('.dropdown').slideToggle().toggleClass('open');
         
-        // Toggle ARIA states and force false attribute to be present
-        $(this).next('.dropdown').toggleAttribute('aria-expanded', true);
-        
-        // Remove from tabbing order any links in closed dropdowns
         $('.dropdown li a').attr('tabindex', -1);
-        
-        // When opening, links are added back to the tabbing order
         $('.dropdown.open li a').attr('tabindex', 0);
-    }
+
+        // Toggle ARIA states
+        if ( $(this).next('.dropdown').hasClass('open') ) {
+            $(this).attr('aria-expanded', true);
+        }
+        else {
+            $(this).attr('aria-expanded', false);
+        }
+    });
+
+    // Pressing enter on a focused H2 should toggle open/close
+    $('.dropdown-toggle').on('keydown', function(event) { 
+        if (event.key === "Enter" || event.key === "ArrowDown") {
+            event.preventDefault();
+            $(this).next('.dropdown').slideToggle().toggleClass('open');
+            
+            $('.dropdown li a').attr('tabindex', -1);
+            $('.dropdown.open li a').attr('tabindex', 0);
+            
+            // Toggle ARIA states
+            if ( $(this).next('.dropdown').hasClass('open') ) {
+                $(this).attr('aria-expanded', true);
+            }
+            else {
+                $(this).attr('aria-expanded', false);
+            }
+        }
+    });
+
+    // Close the dropdown on Esc (anywhere in the document)
+    $(document).on('keydown', function(event) {
+        if (event.keyCode == 27) {
+            event.preventDefault();
+            $('.dropdown-toggle[aria-expanded=true]').each(function() {
+                $(this).next('.dropdown').slideToggle().toggleClass('open');
+                $('li a', $(this).next('.dropdown')).attr('tabindex', -1);
+                // Toggle ARIA states
+                $(this).attr('aria-expanded', false);
+            });
+        }
+    });
 });
